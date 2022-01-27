eee956bdaf75255dbd441a51cb48a5bbbeb593a1
eduardonk9999 <thiago.novak7@gmail.com>
2022-01-27 10:37:08 -0300

    Add, tdd

---
+++ tests/main.spec.js
@@ -0,0 +1,23 @@
+describe('Main', function() {
+  describe('Methodo A', function() {
+    context('Case 1', function() {
+      it('should happen blabla', function() {
+
+      })
+    })
+
+    context('Case 2', function() {
+      it('should happen blabla', function() {
+
+      })
+
+      it('should happen mimimi', function() {
+        
+      })
+    })
+  })
+
+  describe('Method B', function() {
+
+  })
+})
